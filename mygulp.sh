e1d00b0618f1de1fbf4de823d4b04dd955d3ca81
Czipperz <czipperz@gmail.com>
2015-04-03 17:15:01 -0700

    Fixed gulp dir

--- mygulp.sh
+++ mygulp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #stylus
-stylus < scss/compiler.scss > style.css
+stylus < sass/compiler.scss > style.css
 #haml
 for f in $(eval ls haml/ | egrep *.haml); do
 	#compiles all the haml files in the haml/ directory into the base directory as html files.
