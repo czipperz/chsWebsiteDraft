cfb6bcf6573a7130729699ae5f474bbf9a7115ea
Czipperz <czipperz@gmail.com>
2015-04-03 17:09:25 -0700

    Changed gulp to use sass

--- mygulp.sh
+++ mygulp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #stylus
-stylus < stylus/compiler.styl > style.css
+stylus < scss/compiler.scss > style.css
 #haml
 for f in $(eval ls haml/ | egrep *.haml); do
 	#compiles all the haml files in the haml/ directory into the base directory as html files.
